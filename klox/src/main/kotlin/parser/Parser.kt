9c04b708cab41636ed1b891d160d182e1e76e62f
sobatha <natsuki.yokomizo.ny@gmail.com>
2024-09-27 22:26:30 +0900

    chapter6 Parser

---
+++ klox/src/main/kotlin/parser/Parser.kt
@@ -0,0 +1,125 @@
+package org.example.Parser
+
+import org.example.abstractSyntaxTree.Expr
+import org.example.lexer.Token
+import org.example.lexer.TokenType
+import org.example.lexer.TokenType.*
+
+
+class Parser(private val tokens: List<Token>) {
+    private var current = 0
+
+    private fun expression(): Expr = equality()
+
+    private fun equality(): Expr {
+        var expr = comparison()
+        while (match(BANG_EQUAL, EQUAL_EQUAL)) {
+            val operator = previous()
+            val right = comparison()
+            expr = Expr.Binary(expr, operator, right)
+        }
+        return expr
+    }
+
+    private fun comparison(): Expr {
+        var expr: Expr = term()
+
+        while (match(TokenType.GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+            val operator = previous()
+            val right: Expr = term()
+            expr = Expr.Binary(expr, operator, right)
+        }
+
+        return expr
+    }
+
+    private fun term(): Expr {
+        var expr = factor()
+
+        while (match(MINUS, PLUS)) {
+            val operator = previous()
+            val right = factor()
+            expr = Expr.Binary(expr, operator, right)
+        }
+
+        return expr
+    }
+
+    private fun factor(): Expr {
+        var expr = unary()
+
+        while (match(STAR, SLASH)) {
+            val operator = previous()
+            val right = unary()
+            expr = Expr.Binary(expr, operator, right)
+        }
+        return expr
+    }
+
+    private fun unary(): Expr {
+        while (match(BANG, MINUS)) {
+            val operator = previous()
+            val right = unary()
+            return Expr.Unary(operator, right)
+        }
+
+        return primary()
+    }
+
+    private fun primary(): Expr {
+        if (match(FALSE)) return Expr.Literal(false)
+        if (match(TRUE)) return Expr.Literal(true)
+        if (match(NIL)) return Expr.Literal(null)
+
+        if (match(NUMBER, STRING)) return Expr.Literal(previous().literal)
+        if (match(LEFT_PAREN)) {
+            val expr = expression()
+            consume(RIGHT_PAREN, "Expect ')' after expression.")
+            return Expr.Grouping(expr)
+        }
+
+        throw Exception()
+    }
+
+    private fun match(vararg types: TokenType): Boolean {
+        types.forEach {
+            if (check(it)) {
+                advance()
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun check(type: TokenType): Boolean {
+        if (isAtEnd()) return false
+        return peek().type === type
+    }
+
+    private fun advance(): Token {
+        if (!isAtEnd()) current++
+        return previous()
+    }
+
+    private fun isAtEnd(): Boolean {
+        return peek().type === EOF
+    }
+
+    private fun peek(): Token {
+        return tokens[current]
+    }
+
+    private fun previous(): Token {
+        return tokens[current - 1]
+    }
+
+    private fun consume(type: TokenType, message: String): Token {
+        if (check(type)) return advance()
+        throw Exception(message)
+    }
+
+}
+
+
+
+
